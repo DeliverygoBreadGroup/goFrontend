3f6c724b35ac23d4aebfec1bb4cdb44fb451cb04
Ericxsansouza <erick.sousa@sptech.school>
2023-09-11 17:32:59 -0300

    Desenvolvimento completo para apresentação

---
+++ scripts/pagination.js
@@ -0,0 +1,70 @@
+// Caminhos dos botões de cadastro
+
+function secondPartOfRegistration(){
+    window.location = "./cadastroClienteTwo.html";
+}
+
+// Quando juntar as pastas, colocar uma função backPageOne no cadastroCliente.html 
+
+function thirdPartOfRegistration(){
+    window.location = "./cadastroClienteThree.html";
+}
+
+function backPageOne(){
+    window.location = "./cadastroCliente.html";
+}
+
+function backPageTwo(){
+    window.location = "./cadastroClienteTwo.html";
+}
+
+// Funções de enviar e volta do cadastro de comerciante
+
+function secondPartOfRegistrationMerchant(){
+    window.location = "./cadastroComercianteTwo.html";
+}
+
+// 
+
+function thirsPartOfRegistrationMerchant(){
+    window.location = "./cadastroComercianteThree.html";
+}
+
+function backFirstPartOfRegistrationMerchant(){
+    window.location = "./cadastroComerciante.html";
+}
+
+function backSecondPartOfRegistrationMerchant(){
+    window.location = "./cadastroComercianteTwo.html";
+}
+
+function login(){
+    window.location = "./login.html";
+}
+
+function goToCLient(){
+    window.location = "./Cliente.html";
+}
+
+function goToCommerce(){
+    window.location = "./Comerciante.html";
+}
+
+function backLandingPage(){
+    window.location = "./index.html";
+}
+
+function goToRegisterClient(){
+    window.location = "./cadastroCliente.html";
+}
+
+function goToRegisterCommerce(){
+    window.location = "./cadastroComerciante.html";
+}
+
+function backPageClient(){
+    window.location = "./Cliente.html";
+}
+
+// Fim
+
